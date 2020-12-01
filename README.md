8d05ba8870055f559df14e838d1134837afd3348
Rikard Virta <rikard.virta90@gmail.com>
2020-12-01 16:50:07 +0100

    added dependencies. specifying table names

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # java-ee-examination
 Features todo
-1. Customer can place order
-2. Admin can change order status processed to "true"
-3. 
+- User can register
+- User can place order
+- Warehouse staff can change order status processed to "true"
 
--- src/main/java/se/sysdev/javaeeexamination/model/OrderLine.java
+++ src/main/java/se/sysdev/javaeeexamination/model/OrderLine.java
@@ -1,11 +1,9 @@
 package se.sysdev.javaeeexamination.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "orderlines")
 public class OrderLine {
     @Id
     @GeneratedValue
