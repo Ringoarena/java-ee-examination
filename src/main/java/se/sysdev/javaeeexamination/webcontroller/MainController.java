a5e24b71105aea727ee9a5a973ad833f4568c916
Rikard Virta <rikard.virta90@gmail.com>
2020-12-04 10:16:15 +0100

    fixed registration redirect bug

--- src/main/java/se/sysdev/javaeeexamination/webcontroller/MainController.java
+++ src/main/java/se/sysdev/javaeeexamination/webcontroller/MainController.java
@@ -1,14 +1,20 @@
 package se.sysdev.javaeeexamination.webcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.sysdev.javaeeexamination.dto.UserDto;
+import se.sysdev.javaeeexamination.service.UserService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public String showMainIndex() {
@@ -24,5 +30,14 @@ public class MainController {
     public String showUserRegistrationForm(Model model) {
         model.addAttribute("userdto", new UserDto());
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String handleUserRegistration(@ModelAttribute("userdto") UserDto userDto) {
+        boolean success = userService.registerUser(userDto);
+        if (success) {
+            return "redirect:/registration?success";
+        }
+        return "redirect:/registration?failure";
     }
 }
--- src/main/java/se/sysdev/javaeeexamination/webcontroller/UserController.java
+++ src/main/java/se/sysdev/javaeeexamination/webcontroller/UserController.java
@@ -1,22 +1,15 @@
 package se.sysdev.javaeeexamination.webcontroller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import se.sysdev.javaeeexamination.dto.UserDto;
-import se.sysdev.javaeeexamination.service.UserService;
 
 import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserService userService;
 
     @GetMapping
     public String showUserIndex() {
@@ -27,14 +20,5 @@ public class UserController {
     public String showUserDetails(Principal principal, Model model) {
         model.addAttribute("activeuser", principal);
         return "user/details";
-    }
-
-    @PostMapping("/registration")
-    public String handleUserRegistration(@ModelAttribute("userdto") UserDto userDto) {
-        boolean success = userService.registerUser(userDto);
-        if (success) {
-            return "redirect:/user/registration?success";
-        }
-        return "redirect:/user/registration?failure";
     }
 }
